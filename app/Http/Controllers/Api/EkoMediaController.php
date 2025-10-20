3e554859f296cab16f0a4bb5ea805cb61f983978
arizainalf <arizainalf@gmail.com>
2025-10-20 07:38:33 +0700

    update

--- app/Http/Controllers/Api/EkoMediaController.php
+++ app/Http/Controllers/Api/EkoMediaController.php
@@ -70,8 +70,14 @@ class EkoMediaController extends Controller
      */
     public function tema($jenisTemaId)
     {
-        $tema = Tema::where('jenis_tema_id', $jenisTemaId)->with('jenistema')->withCount('video')->get();
-        return $this->successResponse($tema, 'List of Tema retrieved successfully.');
+        $tema      = Tema::where('jenis_tema_id', $jenisTemaId)->withCount('video')->get();
+        $jenistema = JenisTema::find($jenisTemaId);
+        $data      = [
+            'jenis_tema_id' => $jenisTemaId,
+            'tema'          => $tema,
+            'jenistema'     => $jenistema,
+        ];
+        return $this->successResponse($data, 'List of Tema retrieved successfully.');
     }
 
     /**
